33b23b8110a740d3ed39b8ceef127bcb341e2fb1
yacov <jakoff@gmail.com>
2017-03-26 21:50:23 +0300

    Added tests to testng

--- src/test/java/com/vinokur/GoogleSearchWixTest.java
+++ src/test/java/com/vinokur/GoogleSearchWixTest.java
@@ -1,9 +1,5 @@
 package com.vinokur;
 
-import com.vinokur.pages.GoogleSearchPage;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.PageFactory;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 /**
@@ -12,20 +8,10 @@ import org.testng.annotations.Test;
 
 public class GoogleSearchWixTest extends TestNgTestBase {
 
-    private GoogleSearchPage googleSearchPage;
-
-    @BeforeMethod
-    public void initGooglePage() {
-        System.setProperty("webdriver.gecko.driver",
-                "src\\test\\resources\\geckodriver.exe");
-
-        driver = new FirefoxDriver();
-        googleSearchPage = PageFactory.initElements(driver, GoogleSearchPage.class);
-    }
 
     @Test
     public void testGoToWixPage(){
-        driver.get(googleSearchPage.PAGE_URL);
+        driver.get("https://www.google.co.il/");
         googleSearchPage.waitUntilGooglePageIsLoaded();
         googleSearchPage.FillSearchGoogleForm("Wix");
         googleSearchPage.waitUntilSearchWixPageIsLoaded();
--- src/test/java/com/vinokur/TestNgTestBase.java
+++ src/test/java/com/vinokur/TestNgTestBase.java
@@ -1,17 +1,16 @@
 package com.vinokur;
 
-import java.io.IOException;
-
-import org.openqa.selenium.WebDriver;
+import com.vinokur.pages.GoogleSearchPage;
+import com.vinokur.util.PropertyLoader;
 import org.openqa.selenium.Capabilities;
-
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-
 import ru.stqa.selenium.factory.WebDriverPool;
 
-import com.vinokur.util.PropertyLoader;
+import java.io.IOException;
 
 /**
  * Base class for TestNG-based test classes
@@ -21,10 +20,11 @@ public class TestNgTestBase {
   protected static String gridHubUrl;
   protected static String baseUrl;
   protected static Capabilities capabilities;
+  public GoogleSearchPage googleSearchPage;
 
   protected WebDriver driver;
 
-  @BeforeSuite
+  @BeforeSuite(alwaysRun = true)
   public void initTestSuite() throws IOException {
     baseUrl = PropertyLoader.loadProperty("site.url");
     gridHubUrl = PropertyLoader.loadProperty("grid.url");
@@ -34,9 +34,10 @@ public class TestNgTestBase {
     capabilities = PropertyLoader.loadCapabilities();
   }
 
-  @BeforeMethod
+  @BeforeMethod(alwaysRun = true)
   public void initWebDriver() {
     driver = WebDriverPool.DEFAULT.getDriver(gridHubUrl, capabilities);
+    googleSearchPage = PageFactory.initElements(driver, GoogleSearchPage.class);
   }
 
   @AfterSuite(alwaysRun = true)
